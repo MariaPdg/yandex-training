a3a86ba8cbd765eb3f05144b9a9fd8c78e65114e
MariaPdg <mpodguzova25@gmail.com>
2021-10-17 08:11:18 +0200

    hw-6B A-quick-search.py: binary search

---
+++ hw-6B/Python/A-quick-search.py
@@ -0,0 +1,39 @@
+def left_binserach(l, r, params):
+    arr, L = params
+    while l < r:
+        m = l + (r - l) // 2
+        if arr[m] >= L:
+            r = m
+        else:
+            l = m + 1
+    return l
+
+
+def right_binserach(l, r, params):
+    arr, R = params
+    while l < r:
+        m = (l + r + 1) // 2
+        if arr[m] <= R:
+            l = m
+        else:
+            r = m - 1
+    return l
+
+
+if __name__ == '__main__':
+
+    """How many numbers in a range [L, R]?"""
+
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    arr.sort()
+    k = int(input())
+    ans = [0] * k
+
+    for i in range(k):
+        L, R = map(int, input().split())
+        left = left_binserach(0, len(arr)-1, (arr, L))
+        right = right_binserach(0, len(arr)-1, (arr, R))
+        if arr[left] >= L and arr[right] <= R:
+            ans[i] = right - left + 1
+    print(*ans)
