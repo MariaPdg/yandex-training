edcceeb877e5a77229c0acfb68482054c7d7e492
MariaPdg <mpodguzova25@gmail.com>
2021-11-18 13:45:13 +0100

    fix hw-7B D-cats.py, solution with 3 events

--- hw-7B/Python/D-cats.py
+++ hw-7B/Python/D-cats.py
@@ -1,38 +1,53 @@
-from collections import Counter
+def count_cats(events, segments):
 
-
-def count_cats(arr, size):
-
-    prefix = [0] * (size + 2)
-    arr_cnt = Counter(arr)
-    for i in range(1, size+2):
-        prefix[i] = prefix[i-1] + arr_cnt[i-1] if i-1 in arr_cnt else prefix[i-1]
-    print(prefix)
-    return prefix
-
-
-def answer(arr, events, max_p, min_p):
-
-    size = max_p - min_p + 1
-    prefix = count_cats(arr, size)
-    return [prefix[e[1]+1] - prefix[e[0]] for e in events]
+    events.sort()
+    count = 0
+    for event in events:
+        if event[1] == -1:
+            segments[event[2]][2] = count
+        elif event[1] == 1:
+            segments[event[2]][2] = count - segments[event[2]][2]
+        else:
+            count += 1
+    return [elem[2] for elem in segments]
 
 
 if __name__ == '__main__':
 
     """The problem is about cats, reformulation: 
-    How many points (arr) does each segment (events[i]) cover?"""
+    How many points (arr) does each segment (events[i]) cover?
+    Events:
+        -1: segment starts
+         1: segment ends
+         0: cat appears
+    Idea: if event[i] == 0 => increase cat counter: count += 1
+          if event[i] == -1 => segments[event[i]][2] = count
+          if event[i] == 1 => segments[event[i]][2] = count - segments[event[2]][2]: 
+          current cat number - previous cat number"""
 
-    n, m = map(int, input().split())
-    events = []
-    arr = [int(x) for x in input().split()]
-    max_point = 0
-    min_point = 10e9
-    for i in range(m):
-        l, r = map(int, input().split())
-        events.append((l, r))
-        max_point = max(max_point, r)
-        min_point = min(min_point, l)
-    res = answer(arr, events, max_point, min_point)
+    with open('input.txt', 'r') as file:
+        lines = file.readlines()
+    n, m = map(int, lines[0].split())
+    events = [(0, 0, 0)] * (2 * m + n)  # cats + segments (x 2: start, end)
+    cats = [int(x) for x in lines[1].split()]
+    segments = [[0, 0, 0]] * m  # l, r, cat_count
+    start, end, cat_event = -1, 1, 0
+    i = 0
+    for c in cats:
+        events[i] = (c, cat_event, -1)
+        i += 1
+
+    idx = 2
+    for j in range(m):
+        l, r = map(int, lines[idx].split())
+        events[i] = (l, start, j)
+        events[i+1] = (r, end, j)
+        segments[j] = [l, r, 0]
+        i += 2
+        idx += 1
+    for i in range(2, m+2):
+        l, r = map(int, lines[i].split())
+
+    res = count_cats(events, segments)
     print(*res)
 
